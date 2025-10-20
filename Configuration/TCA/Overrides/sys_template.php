61400ca470e0bd2cfa5a34861c1b01a32491ddcd
Sven Wappler <wappler@wappler.systems>
2025-10-21 01:18:09 +0200

    updates

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -2,7 +2,5 @@
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-defined('TYPO3_MODE') or die();
-
 ExtensionManagementUtility::addStaticFile('form_mailchimp', 'Configuration/TypoScript', 'Form Mailchimp');
 
