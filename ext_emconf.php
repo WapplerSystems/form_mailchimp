6ba684b325cd6bda71d625a860265745947139ac
Sven Wappler <wappler@wappler.systems>
2026-04-14 11:52:41 +0200

    Update dependencies for TYPO3 14

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,11 +10,11 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Sven Wappler',
     'author_email' => 'typo3YYYY@wappler.systems',
     'author_company' => 'WapplerSystems',
-    'version' => '13.0.0',
+    'version' => '14.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
-            'form' => '13.4.0-13.4.99'
+            'typo3' => '14.0.0-14.4.99',
+            'form' => '14.0.0-14.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
